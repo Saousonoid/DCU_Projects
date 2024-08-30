1b5a9cf9f9a70841e609bce933de3f5242f83b81
Saria <saria.alkhan@gmail.com>
2024-08-30 17:33:18 +0100

    Fixed Batch/Shell scripts error for framingham_demo

--- framingham_demo/fl/run.bat
+++ framingham_demo/fl/run.bat
@@ -1,10 +1,11 @@
 @echo off
+call conda activate fedenv
 set TF_CPP_MIN_LOG_LEVEL=2
 
 :: Run the scripts in parallel
 start "" cmd /c "python server.py test.csv 30 & pause"
-start "" cmd /c "python client.py client1.csv prevalentHyp  & pause"
-start "" cmd /c "python client.py client2.csv TenYearCHD  & pause"
+start "" cmd /c "python client.py client1.csv prevalentHyp 0  & pause"
+start "" cmd /c "python client.py client2.csv TenYearCHD  1 & pause"
 
 :: Wait for all processes to finish
 echo All scripts are running in parallel.
--- framingham_demo/fl/run.sh
+++ framingham_demo/fl/run.sh
@@ -5,8 +5,8 @@ gnome-terminal -- bash -c "python server.py test.csv 30; exec bash"
 
 sleep 5
 
-gnome-terminal -- bash -c "python client.py client1.csv prevalentHyp; exec bash"
+gnome-terminal -- bash -c "python client.py client1.csv 0 prevalentHyp; exec bash"
 
-gnome-terminal -- bash -c "python client.py client2.csv TenYearCHD; exec bash"
+gnome-terminal -- bash -c "python client.py client2.csv 1 TenYearCHD; exec bash"
 
 echo "All scripts are running in parallel."
